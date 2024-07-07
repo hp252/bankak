1618c2a7c4c954574e660f64ae59699665ebfe5d
hp <86504012+hp252@users.noreply.github.com>
2024-07-07 21:51:42 +0300

    Add files via upload

---
+++ bankak/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    screen:{
+      '-2xl':{'max':'1535px'},
+
+      '-xl':{'max':'1279px'},
+
+      '-lg':{'max':'1023px'},
+
+      '-md':{'max':'767px'},
+
+      '-sm':{'max':'639px'},
+
+    },
+    extend: {}
+    ,
+  },
+  plugins: [],
+}
+
